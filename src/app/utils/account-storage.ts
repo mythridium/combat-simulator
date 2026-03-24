f1c5bca8b38ca74227878f4cadb1a426867525ee
EffBureau <francis.bureau2@gmail.com>
2026-03-24 10:08:51 -0400

    add configurable toggle keybind storage and shortcut utility

--- src/app/utils/account-storage.ts
+++ src/app/utils/account-storage.ts
@@ -2,6 +2,7 @@ export enum StorageKey {
     AlternateEquipmentSelector = 'alternate-equipment-selector',
     SmallPlotter = 'small-plotter',
     FilterTargetDropdown = 'filter-target-dropdown',
+    TogglePanelKeybind = 'toggle-panel-keybind',
     Trials = 'trials',
     Ticks = 'ticks',
     PlotSkill = 'plot-skill',
